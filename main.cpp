d4406cba058645d65b9364519411c07cc1135ee8
Braulio Mac <mac_hip3r@macsmac-9.lan>
2020-11-26 14:43:28 -0600

    Creación y edición de comentarios

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 /*
- * Desarrollado por: Baca Barbosa Braulio José.
- * 	 	     Guevara Mosqueda Héctor.
- * 	 	     Hernández Antonio Aldo Isaac.
- * Fecha de creación: 25/11/2020.
- * Descripción: Driver clase Image y estructura Pixel.
- */
 
+	Desarrollado por:
+ 		Baca Barbosa Braulio José.
+		Guevara Mosqueda Héctor.
+		Hernández Antonio Aldo Isaac.
+
+	Fecha de creación: 25/11/2020.
+	Descripción: Driver clase Image y estructura Pixel.
+
+*/
+
+// Dependencia
 #include <iostream>
-
 #include "includes/image.hpp"
 
+// Declaraciones globales
 using namespace std;
 
+// Función principal
 int main( void ) {
+	// Salida exitosa del programa
 	return 0;
 }
